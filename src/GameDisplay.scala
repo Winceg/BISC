e1f22010aae522c865df98febb48aa48784f414c
JohnBlakeVS <jirokomahou@gmail.com>
2025-01-11 21:21:37 +0100

    starting graphicpart part 2 new fork

--- src/GameDisplay.scala
+++ src/GameDisplay.scala
@@ -1,7 +1,16 @@
 import hevs.graphics.FunGraphics
 
 class GameDisplay (val arena : Arena,val sizeFactor : Int){
-  val sizeFactor : Int
+
+  //fonction to kill the usless border
+  val realSizeX : Int = arena.gridSizeX - 2
+  val realSizeY : Int ) arena.gridSizeY -2
+  var arenaGraphik : Array[Array[String]] = Array.ofDim(realSizeX,realSizeY)
+  for (i<- arenaGraphik.indices){
+    for(j <- arenaGraphik(i).indices){
+      arenaGraphik(i)(j) = arena.grid(i+1)(j+1)
+    }
+  }
   val graphikGridX : Int = arena.gridSizeX*sizeFactor
   val graphikGridY : Int = (arena.gridSizeY+2)*sizeFactor
 
@@ -9,7 +18,7 @@ class GameDisplay (val arena : Arena,val sizeFactor : Int){
   for(i<- arena.grid.indices){
     for(j<- arena.grid(i).indices){
 
+
     }
   }
-
 }
